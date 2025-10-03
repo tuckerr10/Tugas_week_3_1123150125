9e5e51cd8b823fb28e2a6f5c43f8a0c5cd2ae5cb
tuckerr10 <muhfrendy90@gmail.com>
2025-10-03 17:20:12 +0700

    Menambah Void Main

--- bin/oop_mahasiswa.dart
+++ bin/oop_mahasiswa.dart
@@ -74,3 +74,39 @@ class MahasiswaAktif extends Mahasiswa implements Pendaftaran {
     print("$nama berhasil daftar mata kuliah $matkul");
   }
 }
+
+// 4. Fungsi main()
+void main() {
+  // Buat object mahasiswa biasa
+  var mhs1 = Mahasiswa(
+    nama: "Moh Frendy Aprianto",
+    nim: "1123150125",
+    jurusan: "Informatika",
+    angkatan: 2022,
+  );
+  mhs1.tampilkanData();
+  print("----------");
+
+  // Buat object asisten dosen
+  var asdos = AsistenDosen(
+    nama: "I Ketut Gunawan",
+    nim: "654321",
+    jurusan: "Sistem Informasi",
+    angkatan: 2021,
+    mataKuliah: "Pemrograman Mobile",
+  );
+  asdos.tampilkanData();
+  print("----------");
+
+  // Mahasiswa aktif implementasi interface
+  var mhs2 = MahasiswaAktif(
+    nama: "Aslan Walker",
+    nim: "112233",
+    jurusan: "Teknik Elektro",
+    angkatan: 2023,
+  );
+  mhs2.daftarMatkul("Algoritma");
+  mhs2.daftarMatkul("Basis Data");
+  mhs2.tampilkanData();
+  print("Matkul yang diambil: ${mhs2.matkulDiambil}");
+}
