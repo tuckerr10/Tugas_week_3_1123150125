dab557cd3f3bab4eb748555f5ef2bf2aeaa8950f
tuckerr10 <muhfrendy90@gmail.com>
2025-10-03 17:13:51 +0700

    Menambah Class Asisten Dosen extends Mahasiswa

--- bin/oop_mahasiswa.dart
+++ bin/oop_mahasiswa.dart
@@ -1,21 +1,49 @@
+// 1. Class Mahasiswa
 class Mahasiswa {
-// Properties (atribut)
-String nama;
-String nim;
-String jurusan;
-int angkatan;
-// Constructor
-Mahasiswa({
-required this.nama,
-required this.nim,
-required this.jurusan,
-required this.angkatan,
-});
-// Method
-void tampilkanData() {
-print("Nama : $nama");
-print("NIM : $nim");
-print("Jurusan : $jurusan");
-print("Angkatan : $angkatan");
+  // Properties (atribut)
+  String nama;
+  String nim;
+  String jurusan;
+  int angkatan;
+
+  // Constructor
+  Mahasiswa({
+    required this.nama,
+    required this.nim,
+    required this.jurusan,
+    required this.angkatan,
+  });
+
+  // Method
+  void tampilkanData() {
+    print("Nama: $nama");
+    print("NIM : $nim");
+    print("Jurusan : $jurusan");
+    print("Angkatan: $angkatan");
+  }
 }
+
+// 2. Inheritance (AsistenDosen extends Mahasiswa)
+class AsistenDosen extends Mahasiswa {
+  String mataKuliah;
+
+  AsistenDosen({
+    required String nama,
+    required String nim,
+    required String jurusan,
+    required int angkatan,
+    required this.mataKuliah,
+  }) : super(
+          nama: nama,
+          nim: nim,
+          jurusan: jurusan,
+          angkatan: angkatan,
+        );
+
+  // Override method
+  @override
+  void tampilkanData() {
+    super.tampilkanData();
+    print("Asisten MK: $mataKuliah");
+  }
 }
